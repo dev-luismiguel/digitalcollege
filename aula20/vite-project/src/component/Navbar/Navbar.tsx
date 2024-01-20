00bc869bd596917430906cdf173c4f362b5d86c6
Luís Miguel <lmigueldev@gmail.com>
2024-01-19 22:12:42 -0300

    filtro finalizado

--- aula20/vite-project/src/component/Navbar/Navbar.tsx
+++ aula20/vite-project/src/component/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
-import { TextLink } from '../TextLink/TextLink';
-import styles from './Navbar.module.css';
+import { TextLink } from "../TextLink/TextLink";
+import styles from "./Navbar.module.css";
+import { NavbarProps } from "./Navbar.props";
 
-export const Navbar = () => {
+export const Navbar: React.FC<NavbarProps> = ({ onFilterChange }) => {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>Logo</div>
+      <input
+        className={styles.inputField}
+        onChange={(e) => onFilterChange(e.target.value)}
+      />
       <div>
         <div className={styles.logo}>Logo</div>
         <div className={styles.menu}>
-          <TextLink>
-            Menu
-          </TextLink>
+          <TextLink>Menu</TextLink>
         </div>
       </div>
     </div>
