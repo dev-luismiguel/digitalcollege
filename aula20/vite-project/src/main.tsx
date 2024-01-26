75eab1052e0ade31144a50497916c9c60af742ba
Luís Miguel <lmigueldev@gmail.com>
2024-01-26 18:59:10 -0300

    fix

--- aula20/vite-project/src/main.tsx
+++ aula20/vite-project/src/main.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import routes from "./routes";
-import { ThemeProvider } from "./contexts/ThemeContext";
-
-const router = createBrowserRouter(routes);
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
